57f3a86fda4e9547ddaec119970c3666d1044874
Weslley Vander <90974762+weslleyg2@users.noreply.github.com>
2022-08-10 19:39:56 -0300

    conclusão do formulário

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,97 @@
+class AuthenticateForm {
+    constructor() {
+        this.form = document.querySelector('.form');
+        this.events();
+    }
+    events() {
+        this.form.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        });
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const authenticateFields = this.isAuthenticate();//campos validos
+        const authenticatePassword = this.authenticatePassword();
 
-    class AuthenticateForm {
-        constructor() {
-            this.form = document.querySelector('.form');
-            this.events();
+        if (authenticateFields && authenticatePassword) {
+            alert('Form Sent');
+            this.form.submit();
         }
-        events() {
-            this.form.addEventListener('submit', e => {
-                this.handleSubmit(e);
-            });
-        }
-        handleSubmit(e) {
-            e.preventDefault();
-            const authenticateFields = this.isAuthenticate();//campos validos
-        }
-        isAuthenticate() {
-            let valid = true;
+    }
+    authenticatePassword() {
+        let valid = true;
 
-            for(let errorText of this.form.querySelectorAll('.error-text')){{
+        const password = this.form.querySelector('.password');
+        const repeatPassword = this.form.querySelector('.repeatPassword');
+
+        if (password.value !== repeatPassword.value) {
+            valid = false;
+
+            this.createError(password, 'Password  and repeat password fields must be the same.');
+            this.createError(repeatPassword, 'Password  and repeat password fields must be the same.');
+        }
+        if (password.value.length < 6 || password.value.length > 12) {
+            valid = false;
+            this.createError(password, 'Password must be between 3 and 12 characters.');
+        }
+        return valid;
+    }
+    isAuthenticate() {
+        let valid = true;
+
+        for (let errorText of this.form.querySelectorAll('.error-text')) {
+            {
                 errorText.remove();
             }
 
-            }
-            for (let fields of this.form.querySelectorAll('.authenticate')) {
-                const label = fields.previousElementSibling.innerText;
-                if(!fields.value){
-                    this.createError(fields, `"${label}" Field cannot be empty.`);//Campo tal não pode esta vazio
-                    valid = false;
-                }
-
-                if(fields.classList.contains('cpf')){
-                    if(!this.validaCPF(fields)) valid = false;
-                }
-                if(fields.classList.contains('users')){
-                    if(!this.validaUser(fields)) valid = false;
-                }
-            }
         }
-        authenticateUser(field){
-            const user = field.value;
-            let valid = true;
-            if(user.length < 3|| user.length > 12){
-                this.createError(field,'Invalid User.');
-                valid =  false;
-            }
-
-            if(!user.match(/[a-zA-Z0-9]+/g)){
-                this.createError(field, 'User Name need have letters and/or numbers.');
+        for (let fields of this.form.querySelectorAll('.authenticate')) {
+            const label = fields.previousElementSibling.innerText;
+            if (!fields.value) {
+                this.createError(fields, `"${label}" Field cannot be empty.`);//Campo tal não pode esta vazio
                 valid = false;
             }
 
-            return valid;
-        }
-        validaCPF(field){
-            const cpf = new ValidaCPF(field.value);
-
-            if(!cpf.valida()){
-                this.createError(field,'Invalid CPF.');
-                return false;
+            if (fields.classList.contains('cpf')) {
+                if (!this.validaCPF(fields)) valid = false;
             }
-            return true;
+            if (fields.classList.contains('users')) {
+                if (!this.authenticateUser(fields)) valid = false;
+            }
+        }
+        return valid;
+    }
+    authenticateUser(field) {
+        const user = field.value;
+        let valid = true;
+
+        if (user.length < 3 || user.length > 12) {
+            this.createError(field, 'User must be between 3 and 12 characters.');
+            valid = false;
         }
 
-        createError(field, msg){
-            const div = document.createElement('div');
-            div.innerHTML = msg;
-            div.classList.add('error-text');
-            field.insertAdjacentElement('afterend',div);
+        if (!user.match(/^[a-zA-Z0-9]+$/g)) {
+            this.createError(field, 'User Name need have letters and/or numbers.');
+            valid = false;
         }
+
+        return valid;
     }
-    const authenticate = new AuthenticateForm();
+    validaCPF(field) {
+        const cpf = new ValidaCPF(field.value);
+
+        if (!cpf.valida()) {
+            this.createError(field, 'Invalid CPF.');
+            return false;
+        }
+        return true;
+    }
+
+    createError(field, msg) {
+        const div = document.createElement('div');
+        div.innerHTML = msg;
+        div.classList.add('error-text');
+        field.insertAdjacentElement('afterend', div);
+    }
+}
+const authenticate = new AuthenticateForm();
